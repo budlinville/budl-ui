61df0cdd4ce6ef6ec0a0a222c93bf250b6918f44
bud linville <budlinville@gmail.com>
2021-01-03 19:08:52 -0600

    Removed gsap and added react-three-fiber

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import { PerspectiveCamera, Scene, WebGLRenderer } from 'three';
 
 const Home = () => {
+	const scene = new Scene();
+	const camera = new PerspectiveCamera(
+		75,	// Field of View
+		window.innerWidth/window.innerHeight,	// Aspect Ratio
+		0.1,	// Near Plane
+		1000	// Far Plame
+	);
+		const renderer = new WebGLRenderer({antialias: true});
+		renderer.setClearColor("#e5e5e5");	// background color
+		renderer.setSize(window.innerWidth, window.innerHeight);
+
+		document.body.appendChild( renderer.domElement );
+
 	return (
 		<div>
 			Home...
