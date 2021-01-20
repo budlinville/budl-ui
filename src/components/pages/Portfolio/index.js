70a9c99cfd7302a83994d8dfcb2ad144400817e9
bud linville <budlinville@gmail.com>
2021-01-20 16:19:50 -0600

    moved some styles to material-ui's makeStyles hook for portfolio

--- src/components/pages/Portfolio/index.js
+++ src/components/pages/Portfolio/index.js
@@ -1,34 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
 import Title from './Title';
 import profile from '../../../resources/profile.jpg';
 
-class Portfolio extends Component {
-	state = { displayBio: false };
-
-	toggleDisplayBio = () => {
-		this.setState({ displayBio: !this.state.displayBio });
+const Portfolio = () => {
+	const toggleDisplayBio = () => {
+		setDisplayBio(!displayBio);
 	}
 
-	render() {
-		return (
-			<div className='portfolio'>
-				<img src={profile} alt='profile' className="profile" />
-				<h1>Bud Linville</h1>
-				<Title />
-				{ this.state.displayBio ? (
-					<div>
-						<p>Current Softweare Engineer II at Walmart Labs</p>
-						<p>Former Software Engineer Intern at Garmin</p>
-						<button onClick={this.toggleDisplayBio}>Show less</button>
-					</div>
-				) : (
-					<div>
-						<button onClick={this.toggleDisplayBio}>Read more</button>
-					</div>
-				)}
-			</div>
-		)
-	}
+	const [displayBio, setDisplayBio] = useState(false);
+	const classes = useStyles();
+
+	return (
+		<div className={classes.portfolio}>
+			<img src={profile} alt='profile' className={classes.profile} />
+			<h1>Bud Linville</h1>
+			<Title />
+			{ displayBio ? (
+				<div>
+					<p>Current Softweare Engineer II at Walmart Labs</p>
+					<p>Former Software Engineer Intern at Garmin</p>
+					<button onClick={toggleDisplayBio}>Show less</button>
+				</div>
+			) : (
+				<div>
+					<button onClick={toggleDisplayBio}>Read more</button>
+				</div>
+			)}
+		</div>
+	);
 }
+
+// or makeStyles(theme => ({ ...
+const useStyles = makeStyles({
+	portfolio: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-around',
+		alignItems: 'center',
+		marginTop: '20px'
+	},
+	profile: {
+		width: '120px',
+		height: '120px',
+		borderRadius: '60px'
+	}
+});
 
 export default Portfolio;
