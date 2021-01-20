b008a1b00bdcb72b28c4555333762bec425351d8
bud linville <budlinville@gmail.com>
2021-01-20 11:06:50 -0600

    small changes to scene's lighting and camera

--- src/components/three-js/Lighting.js
+++ src/components/three-js/Lighting.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
 const Lighting = () => {
-	const dimensions = useSelector(state => state.scene.dimensions);
+	// Use dimensions only if adding shadow props back
+	// const dimensions = useSelector(state => state.scene.dimensions);
 	return (
 		<group>
-			<ambientLight intensity={0.4}/>
+			<ambientLight intensity={0.75}/>
 			<directionalLight
-				castShadow
 				position={[0, 2, 0]}
-				intensity={2}
-				shadow-mapSize-width={1024}
-				shadow-mapSize-height={1024}
-				shadow-camera-far={dimensions.z}	// TODO: far and near could be backwards
-				shadow-camera-near={-1 * dimensions.z}
-				shadow-camera-left={-1 * dimensions.x}
-				shadow-camera-right={dimensions.x}
-				shadow-camera-top={dimensions.y}
-				shadow-camera-bottom={-1 * dimensions.y}
+				intensity={0.75}
+			/>
+			<directionalLight
+				position={[0, -2, 0]}
+				intensity={0.75}
 			/>
 		</group>
 	);
