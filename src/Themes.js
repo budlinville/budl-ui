fcc1d396977eb2c17cc5ceb5851c5d084ac2ff08
bud linville <budlinville@gmail.com>
2021-01-21 18:43:04 -0600

    Added new themes

--- src/Themes.js
+++ src/Themes.js
@@ -3,9 +3,26 @@ export const light = {
     type: 'light',
   }
 }
-
 export const dark = {
   palette: {
     type: 'dark'
+  }
+}
+export const red = {
+  palette: {
+    primary: 'OrangeRed',
+    secondary: 'MediumSpringGreen'
+  }
+}
+export const blue = {
+  palette: {
+    primary: 'MidnightBlue',
+    secondary: 'Coral'
+  }
+}
+export const purple = {
+  palette: {
+    primary: 'DarkViolet',
+    secondary: 'Gold'
   }
 }
