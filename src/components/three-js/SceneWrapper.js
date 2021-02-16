cbf53121f0bc5b2d8b84fb283422d4343f5eaca2
bud linville <budlinville@gmail.com>
2021-02-16 17:15:29 -0600

    Cleanup

--- src/components/three-js/SceneWrapper.js
+++ src/components/three-js/SceneWrapper.js
@@ -1,27 +1,20 @@
 import React, { useCallback, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import { useTheme } from '@material-ui/styles';
 
-import sphereData from '../../data/spheres';
 import Scene from './Scene';
 import { setOrbitCenter, setCamPos, setSceneDimensions } from '../../store/actions/scene';
 
 const SceneWrapper = ({ history }) => {
 	const classes = useStyles();
 	const dispatch = useDispatch();
-	const hovering = useSelector(state => state.scene.hovering);
-	const primaryBgColor = hovering.length ? sphereData[hovering[0].id].color : '#172837';
-	const secondaryBgColor = hovering.length ? sphereData[hovering[0].id].color2 : '#B5AEAE';
+	const theme = useTheme();
 
 	const navigate = to => {
 		history.push(to);
 	};
-
-	const theme = useTheme();
-	console.log(theme);
-	console.log(theme.palette.secondary.dark);
 
 	const style = useCallback(() => ({
 		background: `linear-gradient(
@@ -29,7 +22,7 @@ const SceneWrapper = ({ history }) => {
 			${theme.palette.primary.main} 50%,
 			${theme.palette.primary.dark} 105%
 		)`
-	}));
+	}), [theme]);
 
 	// SET CONFIGURATIONS FOR SCENE
 	const setSceneState = useCallback(() => {
