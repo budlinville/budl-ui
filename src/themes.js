b438ccc4fd82f7f9c35fd000b3d0560195ce6ad5
bud linville <budlinville@gmail.com>
2021-02-17 21:06:56 -0600

    Added src/themes.js

---
+++ src/themes.js
@@ -0,0 +1,35 @@
+export const light = {
+  palette: {
+    type: 'light',
+  }
+}
+export const dark = {
+  palette: {
+    type: 'dark'
+  }
+}
+export const portfolio = {
+  palette: {
+    primary: { main: '#121443' },
+    secondary: { main: '#46bab3' }
+  }
+}
+export const store = {
+  palette: {
+    primary: { main: '#0A6EE3' },
+    secondary: { main: '#FFB01C' }
+  }
+}
+export const messenger = {
+  palette: {
+    primary: { main: '#7C34E3' },
+    secondary: { main: '#EEFF2C' }
+  }
+}
+
+export const home = {
+  palette: {
+    primary: { main: '#172837' },
+    secondary: { main: '#B5AEAE' }
+  }
+}
