a09d934a7f54ca4817d6e996f45c75f969a97742
bud linville <budlinville@gmail.com>
2021-02-20 17:31:33 -0600

    Fixed Work

--- src/components/pages/Portfolio/Work.js
+++ src/components/pages/Portfolio/Work.js
@@ -5,16 +5,19 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import work from '../../../data/work';
 
-const WorkAccordion = ({ title, description, index }) => {
+const WorkAccordion = ({ position, list, index }) => {
 	const classes = useStyles();
 	const className = index % 2 === 1 ? classes.oddAccordion : classes.evenAccordion;
+	const detailsClassName = index % 2 === 1 ? classes.oddAccordionDetails : classes.evenAccordionDetails;
 	return(
 		<Accordion className={className}>
 			<AccordionSummary expandIcon={ <ExpandMoreIcon /> } aria-controls="panel1a-content">
-				<Typography className={classes.title}> { title } </Typography>
+				<Typography className={classes.title}> { position } </Typography>
 			</AccordionSummary>
-			<AccordionDetails>
-				<Typography className={classes.description}> { description } </Typography>
+			<AccordionDetails className={detailsClassName}>
+				<Typography className={classes.description}>
+					<ul> { list.map( item => ( <li> { item.desc } </li> ))}</ul>
+				</Typography>
 			</AccordionDetails>
 		</Accordion>
 	);
@@ -23,7 +26,7 @@ const WorkAccordion = ({ title, description, index }) => {
 const Work = () => {
 	const classes = useStyles();
 	const workAccordions = work.map((w, i) => (
-		<WorkAccordion title={w.title} description={w.description} index={i} key={i} />
+		<WorkAccordion position={w.position} list={w.list} index={i} key={i} />
 	));
 	return (
 		<div className={classes.rootContainer}>
@@ -41,7 +44,7 @@ const useStyles = makeStyles(theme => ({
 		flexDirection: 'column',
 		justifyContent: 'center',
 		alignItems: 'center',
-		backgroundColor: theme.palette.primary.main,
+		backgroundColor: 'white',
 		padding: '1rem',
 		paddingBottom: '3rem'
 	},
@@ -50,9 +53,9 @@ const useStyles = makeStyles(theme => ({
 		maxWidth: '50rem'
 	},
 	heading: {
-		color: 'white',
+		color: theme.palette.primary.main,
 		fontSize: 40,
-		textShadow: `2px 2px 5px ${theme.palette.secondary.light}`,
+		textShadow: `1px 1px 1px ${theme.palette.secondary.main}`,
 		marginBottom: '1rem'
 	},
 	title: {
@@ -60,14 +63,23 @@ const useStyles = makeStyles(theme => ({
 		textShadow: `0 0 25px ${theme.palette.secondary.light}`,
 	},
 	description: {
-
+		color: 'white'
 	},
 	oddAccordion: {
-		color: theme.palette.primary.dark
+		backgroundColor: theme.palette.primary.main,
+		color: 'white'
 	},
 	evenAccordion: {
 		backgroundColor: theme.palette.secondary.main,
 		color: 'white'
+	},
+	oddAccordionDetails: {
+		backgroundColor: theme.palette.primary.light,
+		color: 'black'
+	},
+	evenAccordionDetails: {
+		backgroundColor: theme.palette.secondary.light,
+		color: 'black'
 	}
 }));
 
--- src/data/work.js
+++ src/data/work.js
@@ -1,15 +1,34 @@
 const work = [
 	{
-		title: 'Walmart',
-		description: 'Software Engineer II'
+		company: 'Walmart',
+		position: 'Software Engineer II',
+		list: [
+			{ desc: 'Upgraded Walmart’s legacy order management systems (OMS)' },
+			{ desc: 'Onboarded more than 6,000 suppliers to Walmart’s new OMS, Nova, allowing both replenishment managers and suppliers to create purchase orders in a single application' },
+			{ desc: 'Designed and developed scalable user-interface components using cutting edge technologies such as React and Redux' },
+			{ desc: 'Assisted business users, monitored application health, and deployed live production fixes during 24/7 on-call support' },
+			{ desc: 'Tested and deployed new application functionality to various environments in Walmart’s cloud-based deployent system, OneOps, in coordination with OMS’s CI/CD effort' }
+		]
 	},
 	{
-		title: 'Garmin',
-		description: 'Software Engineer Intern'
+		company: 'Garmin',
+		position: 'Software Engineer Intern',
+		list: [
+			{ desc: 'Performed maintenance on Garmin’s personal navigation devices (PNDs) by finding and fixing reported bugs in the operating system, written in C/C++' },
+			{ desc: 'Developed new functionality such as toggling PND application availability based on GPS location' },
+			{ desc: 'Pulled in cross-product fixes from similar devices using git cherry-pick' },
+			{ desc: 'Developed a new PND software/file-system update for an internal developer tool, written in C#' },
+			{ desc: 'Upheld Garmin’s coding standards by providing code reviews through Gerrit for five other interns on team' }
+		]
 	},
 	{
-		title: 'University of Kansas',
-		description: 'Undergraduate Teaching Assistant'
+		company: 'University of Kansas',
+		position: 'Teaching Assistant (Programming II)',
+		list: [
+			{ desc: 'Maintained weekly office hours to educate undergraduates on course material including key C++ concepts and data structure implementation' },
+			{ desc: 'Lead lecture hall of ~50 undergraduate in professor’s absence' },
+			{ desc: 'Assisted undergraduates in classroom work by guiding in-class group assignments' }
+		]
 	}
 ];
 
